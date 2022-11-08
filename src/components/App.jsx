6efa14230d13e049740f9477c20728b498e1c72b
Yana Kirdyai <kirdyai@ukr.net>
2022-11-09 01:52:16 +0200

    test

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,10 @@
-import React, { Components } from "react";
-import { Section } from "./Sections/Section";
+import React, { Component } from "react";
+import { Sections } from "./Sections/Sections";
 import { Notification } from "./Notification/Notification";
 import { FeedbackOptions } from "./FeedbackOptions/FeedbackOptions";
-import { FeedBackStat } from "./Statistics/FeedBackStat";
+import { Statistics } from "./Statistics/Statistics";
 
-export class App extends Components {
+export class App extends Component {
     state = {
       good: 0,
       neutral: 0,
@@ -28,22 +28,22 @@ export class App extends Components {
   render() {
     const options = Object.keys(this.state);
       return<>
-          <Section title="Please leave feedback">
+          <Sections title="Please leave feedback">
             <FeedbackOptions
               options={options}
               onLeaveFeedback={this.onClick} />
-          </Section>
+          </Sections>
 
-          <Section title="Statistics">
+          <Sections title="Statistics">
             {this.countTotalFeedback() === 0? <Notification message='No feedback given'/>: (<div>
-              <FeedBackStat
+              <Statistics
                 good={this.state.good}
                 neutral={this.state.neutral}
                 bad={this.state.bad}
                 total={this.countTotalFeedback()}
                 positivePercentage={this.countPositiveFeedbackPercentage()}/>
                 </div>)}
-          </Section>
+          </Sections>
       </>
 }
 }
